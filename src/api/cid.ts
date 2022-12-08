3db0c34a119a89b391c9a36a817cd1fbf25f4e6c
Carlos Di Matteo <carlosdima97@gmail.com>
2022-12-07 22:20:41 -0500

    added miner data

--- src/api/cid.ts
+++ src/api/cid.ts
@@ -12,18 +12,19 @@ export class CidAPI {
 			},
 		});
     const miners = data?.MultihashResults[0]?.ProviderResults.map(({Provider}:any)=>({peerId:Provider.ID,addresses:Provider.Addrs}))
-    console.log({miners})
     // get miner id by peer id https://green.filecoin.space/minerid-peerid/api/v1/miner-id?peer_id=QmQzqxhK82kAmKvARFZSkUVS6fo9sySaiogAnx5EnZ6ZmC
     for(const index in miners ){
       const { data } = await axios.get(`https://green.filecoin.space/minerid-peerid/api/v1/miner-id?peer_id=${miners[index].peerId}`, {
-			headers: {
-				'Accept-Language': 'en-US,en;q=0.5',
-				'Accept-Encoding': 'gzip, deflate, br',
-				Connection: 'keep-alive',
-			},
-		});
-    console.log({data})
+        headers: {
+          'Accept-Language': 'en-US,en;q=0.5',
+          'Accept-Encoding': 'gzip, deflate, br',
+          Connection: 'keep-alive',
+        },
+      });
+      console.log({data})
+      miners[index] = {...miners[index],minerId:data[0].MinerId,...data[0] || {}}
     }
+    console.log({miners})
 		return miners;
 	}
 }
