7a70416fd864c52aa38c64684e028c5adc2b71d6
Carlos Di Matteo <carlosdima97@gmail.com>
2022-12-07 21:24:31 -0500

    fix request to be cors safelisted

--- src/api/cid.ts
+++ src/api/cid.ts
@@ -1,17 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 export class CidAPI {
-  public static async get(cid: string) {
-    const { data } = await axios.get(
-      `https://cors-enable.herokuapp.com/cid.contact/cid/${cid}`,
-      {
-        headers: {
-          Accept: "*/*",
-          "Access-Control-Allow-Origin": "*",
-          "Content-type": "application/json",
-        },
-      }
-    );
-    return data;
-  }
+	public static async get(cid: string) {
+		const { data } = await axios.get(`https://cid.contact/cid/${cid}`, {
+			headers: {
+				'Accept-Language': 'en-US,en;q=0.5',
+				'Accept-Encoding': 'gzip, deflate, br',
+				Origin: 'https://cid.place',
+				Connection: 'keep-alive',
+				Referer: 'https://cid.place/',
+			},
+		});
+		return data;
+	}
 }
